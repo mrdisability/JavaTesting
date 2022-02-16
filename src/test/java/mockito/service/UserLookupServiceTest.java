769c95fc90c01e42026e05f2cfa4a374ebf0d03e
King Ioane <thesamoanppprogrammer@gmail.com>
2022-02-16 13:50:52 +1300

    Added test for getRegularUsers() and write comment with space after //

--- src/test/java/mockito/service/UserLookupServiceTest.java
+++ src/test/java/mockito/service/UserLookupServiceTest.java
@@ -13,6 +13,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class UserLookupServiceTest {
     //Shift Command T: Switch back and forth between production code and test
 
@@ -31,19 +34,34 @@ class UserLookupServiceTest {
 
     @Test
     void getRegularUsers() {
-        //arrange
+        // arrange
         List<User> userList = new LinkedList<>();
         userList.add(User.createRegularUser("sam", "password"));
         userList.add(User.createRegularUser("john", "password"));
-        userList.add(User.createRegularUser("matt", "password"));
+        userList.add(User.createAdminUser("admin", "password"));
 
         Mockito.when(userRepository.findAll()).thenReturn(userList);
 
-        //act
+        // act
         Set<User> actualUsers = userLookupService.getRegularUsers();
 
-        //assert
-        //2 options can use junit or hamcrest assertions
+        // assert - general collection
+        // 2 options can use junit or hamcrest assertions
+        assertNotNull(actualUsers);
+        assertEquals(actualUsers.size(), 2);
+
+        // check user 1
+        User actualUser1 = userList.get(0);
+        assertNotNull(actualUser1);
+        assertEquals(actualUser1.getUserType(), User.UserType.REGULAR_USER);
+        assertEquals(actualUser1.getUsername(), "sam");
+        assertEquals(actualUser1.getPassword(), "password");
+
+        User actualUser2 = userList.get(1);
+        assertNotNull(actualUser2);
+        assertEquals(actualUser2.getUserType(), User.UserType.REGULAR_USER);
+        assertEquals(actualUser2.getUsername(), "john");
+        assertEquals(actualUser2.getPassword(), "password");
     }
 
     @Test
